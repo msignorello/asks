dd1236ac92a492528525ede73a86601115de8bd1
pinwiki <pinwiki@pinwiki.com>
2012-11-06 01:49:36 +0000

    File to hold all functions.

---
+++ includes/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function doublemax($mylist){
+  $maxvalue=max($mylist);
+  while(list($key,$value)=each($mylist)){
+    if($value==$maxvalue)$maxindex=$key;
+  }
+  return array("m"=>$maxvalue,"i"=>$maxindex);
+}
+
+?>
