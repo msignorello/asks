a1615abc6f4e795306a4d2c8c3241da4ee717ece
msignorello <msignorello@gmail.com>
2012-11-14 21:25:06 +0000

    Edited

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <div align="center" class="footer">
-	copyright 2012 - matt signorello - distributed under GNU GPL v3
+	copyright 2012 - ASKS Project Contributors - distributed under GNU GPL v3
 	<br>
 	your ip address: <?php echo $_SERVER['REMOTE_ADDR'];?>
 	<br>
-	http://dipswitch.com/asks
+  https://code.google.com/p/asks
 </div>
