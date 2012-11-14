002fee2de7a9080fa5139eb3bcea8b25121537db
msignorello <msignorello@gmail.com>
2012-11-14 21:25:40 +0000

    Updated Version number.

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <table class="header" width="100%">
 	<tr><td>Today is: <?php echo date("l F dS Y");	?></td>
-	<td align="right">v0.0.2 </td></tr>
+	<td align="right">v0.0.5 </td></tr>
 
 	<tr><td>Local Time: <?php echo date("h:i:s A");?></td>
 	<td align="right">
