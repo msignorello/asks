155f0bf4899620dc696c63c398489d8bc76ec7ba
msignorello <msignorello@gmail.com>
2012-11-25 13:54:37 +0000

    Added link for news management.

--- includes/adminsidenav.php
+++ includes/adminsidenav.php
@@ -2,6 +2,7 @@
 	<div>
 	<ul>
 		<li><a href="sitesettings.php">Site Settings</a></li>
+	  <li><a href="managenews.php">Manage News</a></li>
 		<li><a href="adminsettings.php">Administration</a></li>
 		<li><a href="addgame.php">Add A Game</a></li>		
 		<li><a href="delgame.php">Delete A Game</a></li>
