30ef849c17e1cef851e3419abd4bb9897b00033a
msignorello <msignorello@gmail.com>
2012-11-26 16:47:55 +0000

    Modified to work with game edit system. quick and dirty will have to revisit later to clean up.

--- addgame.php
+++ addgame.php
@@ -36,49 +36,72 @@
 						<table cellspacing="0" cellpadding="0" width="100%" class="content">
 							<tr><td><div class="title">Add A Game<Br></div></td></tr>
 							<tr><td><div class="subtitle">Register a game to record high scores for.</div></td></tr>
+							
 							<tr><td>
 							<div><br>
+						
+<?php 
+							
+							include("includes/adminconnect.inc"); 
+							
+							if(isset($_REQUEST["mode"])){
+
+							$editid = $_SESSION['editgameid'];
+							$gamequery = mysql_query('SELECT * FROM games WHERE gameid = '.$editid.'') or die(mysql_error());
+
+							while($row = mysql_fetch_array( $gamequery )){
+
+							$gameid=$row['gameid'];
+							$gamename=$row['gamename'];
+							$gamelives=$row['livespercredit'];
+							$gametype=$row['gametype'];
+							
+							echo '
+										<form id="newgame" method="post" action="utilprogs/gamemod.php?mode=edit">
+														<div>Game Name: <input type="text" name="game" value="'.$gamename.'"/></div>
+														<div>Lives / Plays Per Credit: <input type="text" size="3" maxlength="1" name="livespercred" value="'.$gamelives.'"/></div>
+														<div>Game Type: 
+												<select name="gametype" value="'.$gametype.'">
+       								<option value="upright">Upright</option>
+  													<option value="cocktail">Cocktail</option>
+														<option value="cabaret">Cabaret</option>
+														<option value="bartop">Bartop</option>
+														<option value="cockpit">Cockpit</option>
+														<option value="pinball">Pinball</option>
+												</select>
+												</div>
+												<br>
+												<input type="submit" name="submit" value="Submit"/>
+									</form>
+							';
+
+							}}else{							
+							
+							echo '
+										<form id="newgame" method="post" action="utilprogs/gamemod.php?mode=add">
+														<div>Game Name: <input type="text" name="game"/></div>
+														<div>Lives / Plays Per Credit: <input type="text" size="3" maxlength="1" name="livespercred"/></div>
+														<div>Game Type: 
+												<select name="gametype">
+       								<option value="upright">Upright</option>
+  													<option value="cocktail">Cocktail</option>
+														<option value="cabaret">Cabaret</option>
+														<option value="bartop">Bartop</option>
+														<option value="cockpit">Cockpit</option>
+														<option value="pinball">Pinball</option>
+												</select>
+												</div>
+												<br>
+												<input type="submit" name="submit" value="Submit"/>
+									</form>
+							';
+									
+									};				
+						?>									
+
+
 
 <!-- -->
-<?php
-
-include("includes/sqlconnect.inc");
-
-if(isset($_POST['submit'])) {
-	$gamename = trim($_POST['game']);
-	$lives = trim($_POST['livespercred']);
-	$typeofgame = trim($_POST['gametype']);
-	
-$query="INSERT INTO games(gamename,livespercredit,gametype) VALUES ('$gamename','$lives','$typeofgame')";
-
-if (!mysql_query($query)){
-	die('Error: ' . mysql_error());
-	}
-
-echo "game added";
-echo '<META HTTP-EQUIV=Refresh CONTENT="2; URL=gamelist.php">';
-}
-
-?>
-
-<form id="newgame" method="post" action="addgame.php">
-	<div>Game Name: <input type="text" name="game"/></div>
-	<div>Lives / Plays Per Credit: <input type="text" size="3" maxlength="1" name="livespercred"/></div>
-	<div>Game Type: 
-		<select name="gametype">
-       <option value="upright">Upright</option>
-  		<option value="cocktail">Cocktail</option>
-			<option value="cabaret">Cabaret</option>
-			<option value="bartop">Bartop</option>
-			<option value="cockpit">Cockpit</option>
-			<option value="pinball">Pinball</option>
-		</select>
-	</div>
-	<br>
-	<input type="submit" name="submit" value="Submit"/>
-</form>
-
-
 							</td></tr></div>
 
 
