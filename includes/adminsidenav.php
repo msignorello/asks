2a04b59a9e269ff38d1fa8639f2467b602f61ff3
msignorello <msignorello@gmail.com>
2012-11-26 16:49:56 +0000

    redesigned admin options based on new pages.

--- includes/adminsidenav.php
+++ includes/adminsidenav.php
@@ -1,10 +1,13 @@
 	<div class="title">Settings</div>
 	<div>
 	<ul>
-		<li><a href="sitesettings.php">Site Settings</a></li>
-	  <li><a href="managenews.php">Manage News</a></li>
-		<li><a href="adminsettings.php">Administration</a></li>
-		<li><a href="addgame.php">Add A Game</a></li>		
-		<li><a href="delgame.php">Delete A Game</a></li>
+		<li><a href="sitesettings.php">Admin Options</a></li>
+	 <li>Manage - </li>
+	 				<ul>
+								<li><a href="managenews.php">News</a></li>
+								<li><a href="managegames.php">Games</a></li>	 		
+								<li><a href="playersettings.php">Players</a></li>	 				
+	 				</ul>
+<!-- 		<li><a href="settings.php">Administration</a></li> -->
 	</ul>
 	</div>
