4daa19ea27574e54bcc62d718465e5a46f46ebe2
pinwiki <pinwiki@pinwiki.com>
2012-12-07 21:04:39 +0000

    Changes theme based on data sent to it.

---
+++ utilprogs/changetheme.php
@@ -0,0 +1,17 @@
+<?php
+	
+	$theme_new= trim($_POST['theme']);
+	
+	$completeurl = "../includes/settings.xml";
+
+	$xml = simplexml_load_file($completeurl);
+
+	$xml->theme = $theme_new;
+
+	$xml->asXML ( '../includes/settings.xml' );
+
+	echo '<META HTTP-EQUIV=Refresh CONTENT="1; URL=../index.php">';
+ 	echo "Theme Changed!";
+	
+	
+?>
