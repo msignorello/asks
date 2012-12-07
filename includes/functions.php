3111df2f59626734684fb3e86942fcaf4901f3fc
pinwiki <pinwiki@pinwiki.com>
2012-12-07 20:07:44 +0000

    Added in a function to set the theme based on the settings file.

--- includes/functions.php
+++ includes/functions.php
@@ -8,4 +8,13 @@ function doublemax($mylist){
   return array("m"=>$maxvalue,"i"=>$maxindex);
 }
 
+function theme_set() {
+	$completeurl = "includes/settings.xml";
+	$xml = simplexml_load_file($completeurl);
+	$theme = $xml->theme;
+	//echo $theme;
+	echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"includes/" . $theme . ".css\"/>" ;
+	}
+
+
 ?>
