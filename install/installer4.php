8663ff28bf15822fbe3762892e05f4916c513d11
pinwiki <pinwiki@pinwiki.com>
2013-05-24 02:47:14 +0000

    Fixed cookies issue.

--- install/installer4.php
+++ install/installer4.php
@@ -1,12 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
- 
-	<link rel="stylesheet" type="text/css" href="../includes/main.css" />
-
-</head>
-<body>
-
 <?php
 session_start(); 
 $_SESSION['pagecount']=4 ; //Set the page we are on. 
@@ -17,7 +8,17 @@ echo 'You are trying to skip steps! This Is BAD!!</br>';
 echo 'Please start from <a href="installer.php">The Beginning</a>';
 die;
 }
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+ 
+	<link rel="stylesheet" type="text/css" href="../includes/main.css" />
 
+</head>
+<body>
+
+<?php
 $checkname = "../install/installed";
 if (!file_exists($checkname)){
 }else{
